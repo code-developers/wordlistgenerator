a1e60441adef23d89efe8b576fcebca43a238d3f
krishpranav <krisna.pranav@gmail.com>
2021-04-18 13:31:35 +0530

    Added user input

--- wordlistgen.py
+++ wordlistgen.py
@@ -22,5 +22,21 @@ print('''\033[91m
 ''')
 
 
+scale = input('\033[36m[!] provide a size scale [eg: "4 to 8" = 4:8] : ')
+start = int(scale.split(':')[0])
+final = int(scale.split(':')[1])
+
+use_nouse = str(input("\n\033[36m[?] Do you want to enter personal data ? [y/N]: "))
+if use_nouse == 'y':
+	first_name = str(input("\n\033[36m[*] Fist Name: "))
+	last_name = str(input("\n\033[36m[*] Last Name: "))
+	birthday = str(input("\n\033[36m[*] Birthday: "))
+	month = str(input("\n\033[36m[*] Month: "))
+	year = str(input("\n\033[36m[*] Year: "))
+	chrs = first_name + last_name + birthday + month + year
+else:
+	chrs = 'abcdefghijklmnopqrstuvwxyz'
+	pass
+
 
 
